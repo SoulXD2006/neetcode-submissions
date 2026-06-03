aeb8b411e6830118171d4d99f4db150f6527b472
Mohammad Mubeen Khan <mmubeen5422@gmail.com>
2026-06-03 18:05:54 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+public:
+    stack<int> s;
+    stack<int> min;
+    MinStack() {
+    }
+
+    void push(int val) {
+        s.push(val);
+        if(min.empty()){min.push(val);}
+        else if(min.top() >= val){min.push(val);}
+    }
+
+    void pop() {
+        if(s.top() == min.top()){min.pop();}
+        s.pop();
+        
+    }
+
+    int top() {
+        return s.top();
+    }
+
+    int getMin() {
+        return min.top();
+    }
+};
